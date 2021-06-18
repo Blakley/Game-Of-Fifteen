0c61de7ef8f062827504abd723a5e6388e60e59f
Anthony Blakley <blakley.ent@gmail.com>
2021-06-18 17:02:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,121 @@
 /* Populate initial table */
-table = document.getElementById("myPuzzle");
+table = document.getElementById("puzzle");
 $(function() {
     resetBoard();
 });
 
+var rotation_state = 0;
+var clockwise_count = 0;
+var flip_count = 0;
+
+function rotate_clockwise() {
+    if (clockwise_count == 0) {
+        rotation_state = 1; // change to clockwise 90 degree state
+        clockwise_count += 1
+
+        // change to active color
+        document.getElementById("btn_rotate90c").setAttribute("style", "border: 2px solid #2EE59D;");
+
+        let n = table.rows.length;
+
+        // 1. take rows and make them columns (transpose our matrix)
+        for (var i = 0; i < n; i++) {
+            for (var j = i; j < n; j++) {
+                let temp = table.rows[i].cells[j].innerHTML;
+                table.rows[i].cells[j].innerHTML = table.rows[j].cells[i].innerHTML;
+                table.rows[j].cells[i].innerHTML = temp;
+            }
+        }
+    
+        // 2. flip horizontally: traverse row from front/back and swap elements until middle element
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < (n/2); j++) {
+                let temp = table.rows[i].cells[j].innerHTML;
+                table.rows[i].cells[j].innerHTML =  table.rows[i].cells[n-1-j].innerHTML;
+                table.rows[i].cells[n-1-j].innerHTML = temp;
+            }
+        }
+        // disable other button
+        document.getElementById("btn_rotate180").disabled = true;
+    }
+    else {
+        // change to inactive color
+        document.getElementById("btn_rotate90c").setAttribute("style", "border: none;");
+
+        // enable other button
+        document.getElementById("btn_rotate180").disabled = false;
+
+        rotation_state = 0; // reset to normal state
+        clockwise_count = 0;
+        resetBoard();
+    }
+    
+    console.log("rotation_state = ", rotation_state);
+}
+
+function rotate_flip() {
+    if (flip_count == 0) {
+        rotation_state = 2; // change to 180 degree state
+        flip_count += 1
+
+        let n = table.rows.length;
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < (n/2); j++) {
+                let temp = table.rows[i].cells[n-j-1].innerHTML;
+                table.rows[i].cells[n-j-1].innerHTML = table.rows[i].cells[j].innerHTML;
+                table.rows[i].cells[j].innerHTML = temp;
+            }
+        }
+        
+        // change to active color
+        document.getElementById("btn_rotate180").setAttribute("style", "border: 2px solid #2EE59D;");
+
+        // enable other button
+        document.getElementById("btn_rotate90c").disabled = true;
+    }
+    else {
+        // enable other button
+        document.getElementById("btn_rotate90c").disabled = false;
+        
+        // change to inactive color
+        document.getElementById("btn_rotate180").setAttribute("style", "border: none;");
+
+        rotation_state = 0; // reset to normal state
+        flip_count = 0;
+        resetBoard();
+    }
+    console.log("rotation_state = ", rotation_state);
+}
 
 /* Reset puzzle board function */
-function resetBoard ()
-{
-    var correct_array =
-    [["1", "2", "3", "4"],
-     ["5", "6", "7", "8"],
-     ["9", "10", "11", "12"],
-     ["13", "14", "15", " "]];
+function resetBoard () {
 
-    for (var i = 0; i < table.rows.length; i++)
-    {
+    var correct_array = [
+        ["1", "2", "3", "4"],
+        ["5", "6", "7", "8"],
+        ["9", "10", "11", "12"],
+        ["13", "14", "15", " "]
+    ];
+
+    rotation_state = 0;
+
+    console.log("rotation_state = ", rotation_state);
+
+    for (var i = 0; i < table.rows.length; i++) {
         for (var j = 0; j < table.rows[i].cells.length; j++)
-        {
             table.rows[i].cells[j].innerHTML = correct_array[i][j];
-        }
+        
     }
 
+    document.getElementById("btn_rotate90c").disabled = false;
+    document.getElementById("btn_rotate180").disabled = false;
+    document.getElementById("btn_rotate90c").setAttribute("style", "border: none;");
+    document.getElementById("btn_rotate180").setAttribute("style", "border: none;");
     resetBoardColor();
 }
 
 /* Event-listener for resetting the table */
-resetButton = document.getElementById("button1");
+resetButton = document.getElementById("btn_reset");
 resetButton.onclick = function(){
     resetBoard(table);
 };
@@ -35,10 +124,8 @@ resetButton.onclick = function(){
 var scramble_level = 20; // default level
 var radios = document.forms["rButtons"].elements["scramble_value"];
 for(var i = 0; i < radios.length; i++) {
-    radios[i].onclick = function()
-    {
-        switch(this.value)
-        {
+    radios[i].onclick = function() {
+        switch(this.value) {
             case "low":
                 scramble_level = 5;
                 break;
@@ -61,10 +148,8 @@ function locateCells() {
     left = null;
     right = null;
 
-    for (var i = 0; i < table.rows.length; i++)
-    {
-        for (var j = 0; j < table.rows[i].cells.length; j++)
-        {
+    for (var i = 0; i < table.rows.length; i++) {
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
             if (table.rows[i].cells[j].innerHTML == " ") {
 
                 current_row = table.rows[i].cells[j].parentElement.rowIndex;
@@ -94,8 +179,7 @@ function locateCells() {
 }
 
 /* Scramble the table function */
-function scrambleTable(up, down, left, right)
-{
+function scrambleTable(up, down, left, right) {
     /* Check which cells are clickable. */
     var items = [];
 
@@ -114,18 +198,16 @@ function scrambleTable(up, down, left, right)
 }
 
 /* Event-listener for scrambling the table */
-scrambleButton = document.getElementById("button2");
+scrambleButton = document.getElementById("btn_scramble");
 scrambleButton.onclick = function() {
     for (var i = 0; i < scramble_level; i++) {
         locateCells();
     }
 };
 
-/* Event-listener for selecting a cell to move */
-for (var i = 0; i < table.rows.length; i++)
-{
-    for (var j = 0; j < table.rows[i].cells.length; j++)
-    {
+// Event-listener for selecting a cell to move 
+for (var i = 0; i < table.rows.length; i++) {
+    for (var j = 0; j < table.rows[i].cells.length; j++) {
         table.rows[i].cells[j].onclick = function() {
 
             current_row = this.parentElement.rowIndex;
@@ -158,8 +240,7 @@ for (var i = 0; i < table.rows.length; i++)
 }
 
 /* Function to swap cells */
-function swapCells(cell, up, down, left, right)
-{
+function swapCells(cell, up, down, left, right) {
     var swappable = 0;
     if (up !=null && up.innerHTML === " ") {
         swappable = 1;
@@ -200,11 +281,40 @@ function swapCells(cell, up, down, left, right)
 
 /* Function to check if the puzzle's been solved. */
 function puzzleCompleted() {
-    var correct_array =
-    [["1", "2", "3", "4"],
-    ["5", "6", "7", "8"],
-    ["9", "10", "11", "12"],
-    ["13", "14", "15", " "]];
+    var correct_array = [
+        ["1", "2", "3", "4"],
+        ["5", "6", "7", "8"],
+        ["9", "10", "11", "12"],
+        ["13", "14", "15", " "]
+    ];
+
+    if (rotation_state == 0) 
+        correct_array =
+        [
+            ["1", "2", "3", "4"],
+            ["5", "6", "7", "8"],
+            ["9", "10", "11", "12"],
+            ["13", "14", "15", " "]
+        ];
+    if (rotation_state == 1)
+        correct_array =
+        [
+            ["13", "9", "5", "1"],
+            ["14", "10", "6", "2"],
+            ["15", "11", "7", "3"],
+            [" ", "12", "8", "4"]
+        ];
+
+    if (rotation_state == 2)
+        correct_array =
+        [
+            ["4", "3", "2", "1"],
+            ["8", "7", "6", "5"],
+            ["12", "11", "10", "9"],
+            [" ", "15", "14", "13"]
+        ];
+
+    console.log("rotation_state = ", rotation_state);
 
     for (var i = 0; i < table.rows.length; i++)
     {
@@ -227,11 +337,11 @@ function puzzleCompleted() {
 /* Puzzle Winner */
 function puzzleWinner() {
     for (var i = 0; i < 16; i++)
-        document.getElementsByTagName('td')[i].setAttribute("style", "color:black; border: 1px solid #6CDA4F;");
+        document.getElementsByTagName('td')[i].setAttribute("style", "background-color:green; border: 2px solid white;");
 }
 
 /* Function to reset puzzle board color. */
 function resetBoardColor() {
     for (var i = 0; i < 16; i++)
-        document.getElementsByTagName('td')[i].setAttribute("style", "color:black; border: 1px solid black;");
+        document.getElementsByTagName('td')[i].setAttribute("style", "border: 2px solid whitesmoke;");
 }
